d6cd12495f841d1ff1863e77077e57f2cff6a67a
seungjin <xeungjin@nate.com>
2026-08-13 14:28:50 +0900

    initial commit

---
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Person Controller - /";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "Person Controller - /user";
+    }
+}
